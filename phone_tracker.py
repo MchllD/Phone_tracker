1a9206f2a535352f20940fdb67ed57fde6a6c357
MchllD <mich11dimafelix@gmail.com>
2024-02-08 00:44:54 +0800

    flow

--- phone_tracker.py
+++ phone_tracker.py
@@ -10,3 +10,17 @@ from tkinter import messagebox
 
 root = tkinter.Tk()
 root.geometry("500x500")
+
+label1 = Label(text="Phone Number Tracker")
+label1.pack()
+
+number = Text(height=1)
+number.pack()
+
+button = Button(text="Search")
+button.pack(pady = 10, padx=100)
+
+result = Text(height=7)
+result.pack()
+
+root.mainloop()
