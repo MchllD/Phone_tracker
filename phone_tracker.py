02b282f242234ee3f8f56563e256f155a99fe4c7
MchllD <mich11dimafelix@gmail.com>
2024-02-08 00:34:42 +0800

    import

---
+++ phone_tracker.py
@@ -0,0 +1,12 @@
+import tkinter 
+import tkintermapview
+import phonenumbers
+
+from phonenumbers import geocoder
+from phonenumbers import carrier 
+
+from tkinter import *
+from tkinter import messagebox
+
+root = tkinter.Tk()
+root.geometry("500x500")
