78f50fdbab4238d47aa70c20754b9022004b6720
MchllD <mich11dimafelix@gmail.com>
2024-02-08 01:00:22 +0800

    import opencagegeocode

--- phone_tracker.py
+++ phone_tracker.py
@@ -1,12 +1,17 @@
 import tkinter 
 import tkintermapview
 import phonenumbers
+import opencage
+
+from key import key
 
 from phonenumbers import geocoder
 from phonenumbers import carrier 
 
 from tkinter import *
 from tkinter import messagebox
+
+from opencage.geocoder import OpenCageGeocode
 
 root = tkinter.Tk()
 root.geometry("500x500")
@@ -20,6 +25,8 @@ def  getResult():
     
     location = geocoder.description_for_number(num1, "en")
     service_provider = carrier.name_for_number(num1, "en")
+    
+    
     
     result.insert(END, "The country of this number is: " + location)
     result.insert(END, "\nThe sim card of this number is: " + service_provider)
